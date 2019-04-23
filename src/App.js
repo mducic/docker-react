70974f9b901e776e1ef94c339238cc93ef7578d5
mducic <mirko.ducic@telenor.rs>
2019-04-23 15:42:40 +0200

    Added travis deploy commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-           Hi there Saskaaasssa!
+           Pozdrav drugari!
           </p>
           <a
             className='App-link'
